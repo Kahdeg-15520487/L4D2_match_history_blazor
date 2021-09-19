8634f881c323f040a87df954a770095c15ef4939
Merlin Nguyen <jonhenry13241@gmail.com>
2021-09-19 17:06:59 +0700

    initial commit

---
+++ L4D2_match_history/Server/Controllers/MatchInfoController.cs
@@ -0,0 +1,30 @@
+﻿using L4D2_match_history.Shared;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace L4D2_match_history.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MatchInfoController : ControllerBase
+    {
+        private readonly ILogger<MatchInfoController> _logger;
+
+        public MatchInfoController(ILogger<MatchInfoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Content(await System.IO.File.ReadAllTextAsync("l4d2plays.json"), "application/json");
+        }
+    }
+}
---
+++ L4D2_match_history/Shared/MatchInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4D2_match_history.Shared
+{
+    public class MatchInfo
+    {
+        public string MapName { get; set; }
+        public string[] Players { get; set; }
+        public DateTime PlayDate { get; set; }
+        public string VideoUrl { get; set; }
+        public string VideoThumbnail { get; set; }
+        public string GetPlayerNames()
+        {
+            var playerCount = Players.Length;
+            string playerNames;
+            if (playerCount > 2)
+            {
+                playerNames = string.Join(", ", Players.Take(playerCount - 2));
+                playerNames += $" and {Players[playerCount - 1]}";
+            }
+            else
+            {
+                playerNames = string.Join(" and ", Players);
+            }
+            return playerNames;
+        }
+
+        public override string ToString()
+        {
+            return $"{MapName} with {GetPlayerNames()} on {PlayDate.ToShortDateString()}";
+        }
+    }
+}
