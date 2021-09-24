49f91be647ac732bd7e18ff0d68d2d0bd9817b81
Merlin Nguyen <jonhenry13241@gmail.com>
2021-09-24 12:24:10 +0700

    fix time zone

--- L4D2_match_history/Server/Controllers/MatchInfoController.cs
+++ L4D2_match_history/Server/Controllers/MatchInfoController.cs
@@ -30,7 +30,7 @@ namespace L4D2_match_history.Server.Controllers
         {
             if (System.IO.File.Exists("l4d2plays.json"))
             {
-                return Content($"{{\"lastUpdate\":\"{System.IO.File.GetLastWriteTime("l4d2plays.json")}\",\"matchs\":{await System.IO.File.ReadAllTextAsync("l4d2plays.json")}}}", "application/json");
+                return Content($"{{\"lastUpdate\":\"{System.IO.File.GetLastWriteTimeUtc("l4d2plays.json")}\",\"matchs\":{await System.IO.File.ReadAllTextAsync("l4d2plays.json")}}}", "application/json");
             }
             return NoContent();
         }
