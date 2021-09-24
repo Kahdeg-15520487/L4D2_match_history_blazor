22327d70f1a520c324e6674b91afc616d1a2bea1
Merlin Nguyen <jonhenry13241@gmail.com>
2021-09-24 11:13:22 +0700

    add last update time

---
+++ L4D2_match_history/Shared/MatchInfos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4D2_match_history.Shared
+{
+    public class MatchInfos
+    {
+        public string lastUpdate { get; set; }
+        public MatchInfo[] Matchs { get; set; }
+    }
+}
